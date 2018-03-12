9c459077fd1041b242bc09c8cac2e06122ac7de3
americast <sayan.sinha@iitkgp.ac.in>
2018-03-13 01:39:40 +0530

    db connected

---
+++ src/Search.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TeamVideoSummarization
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+public class Search extends HttpServlet {
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        Connection conn = null;
+        String url = "";
+        String dbName = "JIS";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "root";
+        String password = "root";
+ 
+        Statement st;
+        try {
+            Class.forName(driver).newInstance();
+            conn = DriverManager.getConnection(url + dbName, userName, password);
+            System.out.println("Connected!");
+            String pid = request.getParameter("pid");
+ 
+            ArrayList al = null;
+            ArrayList pid_list = new ArrayList();
+            String query = "select * from users where uname='" + pid + "' ";
+ 
+            System.out.println("query " + query);
+            st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+ 
+            while (rs.next()) {
+                al = new ArrayList();
+ 
+//                out.println(rs.getString(1));
+//                out.println(rs.getString(2));
+//                out.println(rs.getString(3));
+//                out.println(rs.getString(4));
+                al.add(rs.getString(1));
+                al.add(rs.getString(2));
+                al.add(rs.getString(3));
+                al.add(rs.getString(4));
+ 
+                System.out.println("al :: " + al);
+                pid_list.add(al);
+            }
+ 
+            request.setAttribute("piList", pid_list);
+            RequestDispatcher view = request.getRequestDispatcher("/searchview.jsp");
+            view.forward(request, response);
+            conn.close();
+            System.out.println("Disconnected!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
