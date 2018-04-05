cd1e467ee09d862dfc7a4f4eb0971647f92d0781
americast <sayan.sinha@iitkgp.ac.in>
2018-04-05 23:04:58 +0530

    HttpUnit test done

--- HttpUnit/Example.java
+++ HttpUnit/Example.java
@@ -21,6 +21,33 @@ GetMethodWebRequest( "http://localhost:8080/JIS/index.html" );
   //Display the content of the page
   System.out.println("Page content is :" + response.getText() );
 
+  WebForm loginForm = response.getForms()[0];
+  request = loginForm.getRequest();
+  request.setParameter( "username", "b" );
+  request.setParameter( "password", "b" );
+  response = wc.getResponse( request );
+  System.out.println("Title is :" + response.getTitle() );
+  System.out.println("Page content is :" + response.getText() );
+
+  loginForm = response.getForms()[0];
+  request = loginForm.getRequest();
+  response = wc.getResponse( request );
+
+  System.out.println("Title is :" + response.getTitle() );
+  System.out.println("Page content is :" + response.getText() );
+
+  loginForm = response.getForms()[1];
+  request = loginForm.getRequest();
+  response = wc.getResponse( request );
+
+  System.out.println("Title is :" + response.getTitle() );
+  System.out.println("Page content is :" + response.getText() );
+  String text = response.getText();
+
+  if (text.contains("Mar 14, 2018 8:06:04 PM"))
+	System.out.println("Found");
+
+
   } catch (Exception e) {
   System.err.println( "Exception: " + e );
   }
